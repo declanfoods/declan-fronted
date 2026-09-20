25af7acca45119da7ecd177fd2632d35c8fb5d9c
Odebode Oluwatimilehin <odebodezion@gmail.com>
2026-09-20 22:04:59 +0100

    add pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(), 
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false,
+      workbox: {
+        navigateFallback: '/index.html',
+      },
+    }),
+  ],
   server: {
     port: 5173,
   },
